51dba583b121b30485cbaaa988790dbdeb483d18
omrobbie <putu.roby@yahoo.com>
2018-08-11 11:41:53 +0700

    penggunaan Icon

--- tutorial_1/lib/main.dart
+++ tutorial_1/lib/main.dart
@@ -10,15 +10,20 @@ class HomeApp extends StatelessWidget {
     return MaterialApp(
       title: "Basic Widget",
       home: Scaffold(
-        body: Center(
-            child: Text(
-          "Hello Flutter",
-          style: TextStyle(
-              color: Colors.green[600],
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold),
-        )),
-      ),
+          body: Center(
+        //     child: Text(
+        //   "Hello Flutter",
+        //   style: TextStyle(
+        //       color: Colors.green[600],
+        //       fontSize: 24.0,
+        //       fontWeight: FontWeight.bold),
+
+        child: Icon(
+          Icons.android,
+          size: 70.0,
+          color: Color.fromARGB(255, 66, 165, 245),
+        ),
+      )),
     );
   }
 }
