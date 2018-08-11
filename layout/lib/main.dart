cd003a50701346a45d7ddf254092e4d9c8b99160
omrobbie <putu.roby@yahoo.com>
2018-08-11 13:37:32 +0700

    penggunaan MainAxisAlignment

--- layout/lib/main.dart
+++ layout/lib/main.dart
@@ -15,8 +15,23 @@ class LayoutApp extends StatelessWidget {
           Image.network(
               "https://i1.wp.com/kabar.kibar.co.id/wp-content/uploads/2018/02/ae812da3f28ba5ad03c2da1a7fc7c724.jpg"),
           Container(
-            padding: EdgeInsets.only(top: 10.0),
-            child: Text("Flutter Study Jam - Session 1"),
+            padding: EdgeInsets.only(top: 10.0, left: 10.0),
+            child: Text(
+              "Flutter Study Jam - Session 1",
+              style: TextStyle(fontSize: 20.0),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Icon(Icons.android, size: 40.0),
+                Icon(Icons.home, size: 40.0),
+                Icon(Icons.settings, size: 40.0),
+              ],
+            ),
           ),
         ],
       ),
