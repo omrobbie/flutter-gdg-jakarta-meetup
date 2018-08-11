7f22f849a402a354dd5b6cff06829aa2bd79b5d1
omrobbie <putu.roby@yahoo.com>
2018-08-11 11:26:49 +0700

    penggunaan basic scaffold

--- tutorial_1/lib/main.dart
+++ tutorial_1/lib/main.dart
@@ -8,6 +8,10 @@ class HomeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Basic Widget", home: Container(child: Text("Hello Flutter")));
+      title: "Basic Widget",
+      home: Scaffold(
+        body: Center(child: Text("Hello Flutter")),
+      ),
+    );
   }
 }
