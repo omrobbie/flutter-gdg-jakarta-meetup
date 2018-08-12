9bbdaf2c3eb74679ab3201d3ad04b16725016365
omrobbie <putu.roby@yahoo.com>
2018-08-12 11:26:27 +0700

    tambahkan global key untuk form state

--- blog/lib/rootpage.dart
+++ blog/lib/rootpage.dart
@@ -7,6 +7,7 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
+  final _formKey = GlobalKey<FormState>();
   String email, password;
 
   @override
@@ -14,28 +15,31 @@ class _RootPageState extends State<RootPage> {
     return Scaffold(
       body: Container(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 32.0)),
-            CustomEditText(
-              sLabel: "Email Address",
-              sError: "Please fill your email address",
-              inputType: TextInputType.emailAddress,
-              frmSaved: (value) => email = value,
-            ),
-            CustomEditText(
-              sLabel: "Password",
-              sError: "Please fill your email address",
-              inputType: TextInputType.text,
-              bSecure: true,
-              frmSaved: (value) => password = value,
-            ),
-            CustomButton(
-              sText: "Sign In",
-              callback: () {},
-            ),
-          ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(top: 32.0)),
+              CustomEditText(
+                sLabel: "Email Address",
+                sError: "Please fill your email address",
+                inputType: TextInputType.emailAddress,
+                frmSaved: (value) => email = value,
+              ),
+              CustomEditText(
+                sLabel: "Password",
+                sError: "Please fill your email address",
+                inputType: TextInputType.text,
+                bSecure: true,
+                frmSaved: (value) => password = value,
+              ),
+              CustomButton(
+                sText: "Sign In",
+                callback: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
