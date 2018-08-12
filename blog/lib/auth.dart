06354f94058439d4fd599f8f1cc00a2c1e654d10
omrobbie <putu.roby@yahoo.com>
2018-08-12 14:58:35 +0700

    penggunaan auth

---
+++ blog/lib/auth.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
+abstract class BaseAuth {
+  Future<String> createWithEmailAndPassword(String email, String password);
+}
+
+class Auth {
+  Future<String> createWithEmailAndPassword(
+      String email, String password) async {
+    FirebaseUser user = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return user.uid;
+  }
+}
