53f2d038a5fb0a6d0cf07b77c3d6127513bd20aa
omrobbie <putu.roby@yahoo.com>
2018-08-12 22:46:52 +0700

    inisiasi RootPage

--- blog/lib/main.dart
+++ blog/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'rootPage.dart';
+import 'auth.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RootPage(),
+      home: RootPage(auth: Auth()),
     );
   }
 }
